113d9e496e2cdc96f78d7a4167026f36c65cfb06
Krishna <krishnasudan908@gmail.com>
2021-11-18 12:10:03 +0530

    changed into react

---
+++ src/components/Team.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Team = () => {
+    return (
+        <>
+        <div className="container">
+            This is Team Page
+        </div>
+        </>
+    )
+}
