3c1124950a8e18270be891ad43f9254ddb69a5b7
unknown <sanketpatil8234@gmail.com>
2022-07-31 14:08:38 +0530

    added logo and style the navbar

--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import CsiLogo from './CsiLogo'
 
 const NavbarContainer = (props) => {
   return (
     <nav className="navbarContainer">
+         <CsiLogo />
         <ul className='navbar-ul'>
         {props.children}
         </ul>
