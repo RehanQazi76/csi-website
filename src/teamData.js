da38e212d7fd5f1f7076bbf1af56cfa115bf8a0b
unknown <sanketpatil8234@gmail.com>
2022-07-31 12:25:32 +0530

    changed the navbar and added some style

---
+++ src/teamData.js
@@ -0,0 +1,120 @@
+export const memberInfo = [
+    {
+        id:1,
+        name:"Atharva Joshi",
+        position:"Machine Learning Head",
+        isHead:true,
+        png:"atharva_joshi.png",
+        hoverDescription:"A biological neural network trained for 19 years.",
+        linkedin:"https://www.linkedin.com/in/atharva-joshi-a89a671b2"
+    },
+    {
+        id:2,
+        name:"Himanshu Dhar",
+        position:"Web Development Head",
+        isHead:true,
+        png:"himanshu_dhar.png",
+        hoverDescription:"Have you tried turning it on and off again?",
+        linkedin:"https://www.linkedin.com/in/himanshu-dhar-7411671b2/"
+    },
+    {
+        id:3,
+        name:"Vivek Verma",
+        position:"App Development Head",
+        isHead:true,
+        png:"vivek_verma.png",
+        hoverDescription:"Does what excites",
+        linkedin:"https://www.linkedin.com/in/omegaviv"
+    },
+    {
+        id:4,
+        name:"Aditya Malvadkar",
+        position:"Techincal Coordinator",
+        isHead:false,
+        png:"aditya_malvadkar.png",
+        hoverDescription:"I am  a fast Learner",
+        linkedin:"https://www.linkedin.com/in/aditya-malvadkar-93a6b2221/"
+    },
+    {
+        id:5,
+        name:"Anoushka Mudhkhedkar",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"noushka_mudkhedkar.png",
+        hoverDescription:"Just trying my best",
+        linkedin:"https://www.linkedin.com/in/anoushka-mudkhedkar-2932a720a"
+    },
+    {
+        id:6,
+        name:"Krishna Sudan",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"Krishna_sudan.png",
+        hoverDescription:"A passionate learner who believes that knowledge is never wasted.",
+        linkedin:"https://www.linkedin.com/in/krishna908"
+    },
+    {
+        id:7,
+        name:"Kshitij Chaudhari",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"kshitij_chaudhari.png",
+        hoverDescription:"You can always find me in front of laptop screen, with so many classNamees and functions",
+        linkedin:"https://www.linkedin.com/in/kshitij-chaudhari-87440b206"
+    },
+    {
+        id:8,
+        name:"Mitul Koul",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"mitul_koul.png",
+        hoverDescription:"Want to learn everyThing",
+        linkedin:"https://www.linkedin.com/in/mitulkoul/"
+    },
+    {
+        id:9,
+        name:"Rehanuddin Qazi",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"rehanuddin_qazi.png",
+        hoverDescription:"A person that u can rely on in your every situation ",
+        linkedin:"https://www.linkedin.com/in/rehanuddin-qazi-0a452020a/"
+    },
+    {
+        id:10,
+        name:"Sahil hariyal",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"sahil_hariyal.png",
+        hoverDescription:"I'm creative, hard-working, and detail-oriented.",
+        linkedin:"https://www.linkedin.com/in/sahil-hariyal-957666220"
+    },
+    {
+        id:11,
+        name:"Sanket Jagdale",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"sanket_jagdale.png",
+        hoverDescription:"Someone who has courage to follow his heart and intuition",
+        linkedin:"https://www.linkedin.com/in/sanket-jagdale-09/"
+    },
+    {
+        id:12,
+        name:"Sanket Patil",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"sanket_patil.png",
+        hoverDescription:"I'm passionate about my work.",
+        linkedin:"https://www.linkedin.com/in/sanket-patil-218852207"
+    },
+    {
+        id:12,
+        name:"Sanket Patil",
+        position:"Technical Coordinator",
+        isHead:false,
+        png:"sanket_patil.png",
+        hoverDescription:"I'm passionate about my work.",
+        linkedin:"https://www.linkedin.com/in/sanket-patil-218852207"
+    },
+
+]
